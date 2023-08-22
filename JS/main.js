b2186045512640b637421a27e60d1020a3171d92
Emad Jammal <emad.elqassas.1@gmail.com>
2023-08-22 04:42:07 +0300

    JQuery assignment

---
+++ JS/main.js
@@ -0,0 +1,49 @@
+// NAV 
+$("#bars").click(()=>{
+  $("#nav").animate({left:"0"},700)
+})
+$("#close").click(()=> {
+  let width = $("#nav").innerWidth();
+  $("#nav").animate({left:`-${width}`},700)
+})
+
+
+$(".singer").click(function () {
+  $(this).children('p').slideToggle(); 
+  $(this).siblings().children('p').slideUp();
+});
+
+let counterDownDate = new Date("Dec 31,2025 23:59:59").getTime()
+let counter = setInterval(()=>{
+  let dateNow = new Date().getTime();
+  let dateDiff = counterDownDate - dateNow;
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(dateDiff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60) );
+  let minutes = Math.floor(dateDiff % (1000 * 60 * 60 ) / (1000 * 60));
+  let seconds = Math.floor(dateDiff % (1000 * 60 ) / 1000);
+  $("#days").html(days)
+  $("#hours").html(hours)
+  $("#mins").html(minutes)
+  $("#secs").html(seconds)
+},1000)
+
+
+
+$("#message").on("keyup", function () {
+  
+  let maxLength = 100;
+  $("#countLetters").html(maxLength)
+  let inputValue = $(this).val();
+
+  let inputLength = inputValue.length;
+  let leftLength = maxLength - inputLength;
+  console.log(leftLength);
+  if (leftLength > 0) {
+    $("#countLetters").html(leftLength)
+    } else {
+      $("#counterOfLetter").addClass('d-none');
+      $("#alert").removeClass('d-none')
+    }
+}
+)
+
